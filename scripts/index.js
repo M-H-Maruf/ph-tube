e864141149b9e528ae8a43f1718ef3e2a2c99bf7
M-H-Maruf <maruf.nightwalker@gmail.com>
2023-09-02 05:22:20 +0600

    verified functionality added

--- scripts/index.js
+++ scripts/index.js
@@ -33,12 +33,15 @@ const displayCategories = (categories) => {
 
     categories.forEach((category) => {
         console.log(category);
+
         const categoryButton = document.createElement("button");
+
         categoryButton.classList = `category-btn btn rounded md:text-lg font-medium bg-tube-background text-tube-secondary text-opacity-70 h-fit mb-5`;
         if (category.category_id == "1000") {
             categoryButton.classList.add("active");
         }
         categoryButton.textContent = category.category;
+
         categoryContainer.appendChild(categoryButton);
     });
 };
@@ -63,10 +66,10 @@ const displayVideos = (videos) => {
 
     videos.forEach((video) => {
         console.log(video);
-        // 2 create a div
+
         const videoCard = document.createElement("div");
         videoCard.classList = `card w-80 mb-12 gap-5`;
-        // 3: set inner html
+
         videoCard.innerHTML = `
         <figure class="w-80 h-48">
         <img class="rounded-lg w-full h-full object-cover object-center" src=${video.thumbnail} alt="Shoes" />
@@ -79,14 +82,36 @@ const displayVideos = (videos) => {
                 <h3 class="text-base text-tube-card font-bold mb-2">${video.title}</h3>
                 <div class="flex gap-3">
                     <h2 class="text-sm text-tube-card text-opacity-70 mb-2 inline">${video.authors[0].profile_name}</h2>
-                    <img class="w-5 h-5" src="images/tick.png" alt="verified">
                 </div>
                 <p class="text-sm text-tube-card text-opacity-70">${video.others.views} views</p>
             </div>
         </div>
                 `;
-        // 4 append child
+
+        // verified functionality
+        if (video.authors[0].verified == true) {
+            videoCard.innerHTML = `
+            <figure class="w-80 h-48">
+            <img class="rounded-lg w-full h-full object-cover object-center" src=${video.thumbnail} alt="Shoes" />
+            </figure>
+            <div class="card-body flex flex-row gap-4 p-0">
+                <figure class="w-10 h-10 rounded-full">
+                <img class="rounded-full w-full h-full object-cover object-center" src=${video.authors[0].profile_picture} alt="profile-picture">
+                </figure>
+                <div class="card-text">
+                    <h3 class="text-base text-tube-card font-bold mb-2">${video.title}</h3>
+                    <div class="flex gap-3">
+                        <h2 class="text-sm text-tube-card text-opacity-70 mb-2 inline">${video.authors[0].profile_name}</h2>
+                        <img class="w-5 h-5" src="images/tick.png" alt="verified">
+                    </div>
+                    <p class="text-sm text-tube-card text-opacity-70">${video.others.views} views</p>
+                </div>
+            </div>
+                    `;
+        }
+                
         videoContainer.appendChild(videoCard);
+
     });
 };
 
