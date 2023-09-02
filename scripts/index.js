1fc28a5eced920c0a32bc88e04a9d1cdf16092ec
M-H-Maruf <maruf.nightwalker@gmail.com>
2023-09-02 06:35:39 +0600

    dynamic time stamp added

--- scripts/index.js
+++ scripts/index.js
@@ -71,47 +71,52 @@ const displayVideos = (videos) => {
         videoCard.classList = `card w-80 mb-12 gap-5`;
 
         // calculate time stamp
-        function secondsToTime(seconds) {
-            const days = Math.floor(seconds / 86400); 
-            seconds %= 86400; 
-            const hours = Math.floor(seconds / 3600); 
-            seconds %= 3600; 
-            const minutes = Math.floor(seconds / 60); 
-            seconds %= 60; 
-          
-            // Construct the time string
-            let timeString = "";
+
+        let seconds = parseInt(video.others.posted_date);
+        let days = Math.floor(seconds / 86400);
+        seconds %= 86400;
+        const hours = Math.floor(seconds / 3600);
+        seconds %= 3600;
+        const minutes = Math.floor(seconds / 60);
+        seconds %= 60;
+        const years = Math.floor(days / 365);
+
+        // Construct the time string
+        let timeString = "";
+        if (years > 0) {
+            timeString = years + " years ";
+        } else {
             if (days > 0) {
-              timeString += days + " days ";
+                timeString += days + " days ";
             }
             if (hours > 0) {
-              timeString += hours + " hrs ";
+                timeString += hours + " hrs ";
             }
             if (minutes > 0) {
-              timeString += minutes + " min ";
+                timeString += minutes + " min ";
             }
-            if (seconds > 0) {
-              timeString += seconds + " secs ";
-            }
-          
-            return timeString.trim();
-          }
-          
-          const totalSeconds = parseInt(video.others.posted_date);
-          const formattedTime = secondsToTime(Math.floor(totalSeconds)); 
-          console.log(formattedTime); 
+        }
+        let formattedTime = "";
+        if (seconds > 0 || years > 0) {
+            formattedTime =
+                '<h6 class="absolute bg-tube-card p-1 text-white text-xs rounded right-3 bottom-3 w-fit">' +
+                timeString +
+                " ago</h6>";
+        }
+
+        console.log(formattedTime);
+
         //   verification
-          let verified = '';
-          if (video.authors[0].verified == true) {
-             verified = '<img class="w-5 h-5" src="images/tick.png" alt="verified">';
-          }
+        let verified = "";
+        if (video.authors[0].verified == true) {
+            verified =
+                '<img class="w-5 h-5" src="images/tick.png" alt="verified">';
+        }
 
         videoCard.innerHTML = `
         <figure class="w-80 h-48 relative">
-        <img class="rounded-lg w-full h-full object-cover object-center" src=${video.thumbnail} alt="Shoes" />
-        
-        <h6 class="absolute bg-tube-card p-1 text-white text-xs rounded right-3 bottom-3 w-fit">${formattedTime}</h6>
-
+        <img class="rounded-lg w-full h-full object-cover object-center" src=${video.thumbnail} alt="Shoes"/>        
+        ${formattedTime}
         </figure>
         <div class="card-body flex flex-row gap-4 p-0">
             <figure class="w-10 h-10 rounded-full">
@@ -131,33 +136,7 @@ const displayVideos = (videos) => {
         </div>
                 `;
 
-        // verified functionality
-        if (video.authors[0].verified == true) {
-            // videoCard.innerHTML = `
-            // <figure class="w-80 h-48 relative">
-            // <img class="rounded-lg w-full h-full object-cover object-center" src=${video.thumbnail} alt="Shoes" />
-            
-            // <h6 class="absolute bg-tube-card p-1 text-white text-xs rounded right-3 bottom-3 w-fit">3hrs 56 min ago</h6>
-
-            // </figure>
-            // <div class="card-body flex flex-row gap-4 p-0">
-            //     <figure class="w-10 h-10 rounded-full">
-            //     <img class="rounded-full w-full h-full object-cover object-center" src=${video.authors[0].profile_picture} alt="profile-picture">
-            //     </figure>
-            //     <div class="card-text">
-            //         <h3 class="text-base text-tube-card font-bold mb-2">${video.title}</h3>
-            //         <div class="flex gap-3">
-            //             <h2 class="text-sm text-tube-card text-opacity-70 mb-2 inline">${video.authors[0].profile_name}</h2>
-            //             <img class="w-5 h-5" src="images/tick.png" alt="verified">
-            //         </div>
-            //         <p class="text-sm text-tube-card text-opacity-70">${video.others.views} views</p>
-            //     </div>
-            // </div>
-            //         `;
-        }
-                
         videoContainer.appendChild(videoCard);
-
     });
 };
 
